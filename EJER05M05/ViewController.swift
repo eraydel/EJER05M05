421b5b5bbb9ee06be6ae0032be666e85d320f91a
Erick Ayala <ayala.delgadillo.erick@icloud.com>
2022-04-02 14:14:23 -0600

    Initial Commit

---
+++ EJER05M05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EJER05M05
+//
+//  Created by Erick Ayala Delgadillo on 02/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
